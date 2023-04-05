ab1cdbf649bf9d778d78e72d09cbe93d0752b275
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-04-05 13:05:34 +0700

    membatasi maksimal array sebanyak 20, dan perulangan sebanyak 20

---
+++ Activity5_Structure3/Activity5_Structure3.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+	char Desa[20];
+	char Kota[20];
+};
+struct mahasiswa
+{
+	char nama[20];
+	char nim[12];
+	DetailAlamat alamat;
+	int umur;
+};
+
+
+int main() {
+	mahasiswa mhs[20];
+	for (int i = 0; i < 20; i++)
+	{
+		cout << "masukan nim =";
+		cin.getline(mhs[i].nim, 12);
+		cout << "masukan umur=";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << "masukan nama=";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat=";
+		cout << "\n\tMasukan Desa =";
+		cin.getline(mhs[i].alamat.Desa, 20);
+		cout << "\n\tMasukan Kota =";
+		cin.getline(mhs[i].alamat.Kota, 20);
+	}
+
+	for (int i = 0; i < 20; i++)
+	{
+		cout << "\n\nNIM = " << mhs[i].nim;
+		cout << "\nUmur = " << mhs[i].umur;
+		cout << "\nNama = " << mhs[i].nama;
+		cout << "\nAlamat = ";
+		cout << "\n\tDesa = " << mhs[i].alamat.Desa;
+		cout << "\n\tKota = " << mhs[i].alamat.Kota;
+	}
+
+}
